9997b21fc2d0921d86223adf67966a8937d36b8b
George Madeley <george.madeley@outlook.com>
2023-11-28 12:20:32 +0000

    Fixed Varying Images Error

--- scripts/showImages.py
+++ scripts/showImages.py
@@ -9,22 +9,28 @@ df = pd.read_csv(fileName)
 # get the unique values of the image_name column
 image_names = df.image_name.unique()
 
-# get the unique values of the filter_name column
-filters = df.filter_name.unique()
 
-# get the unique values of the kernel_size column
-kernel_sizes = df.kernel_size.unique()
-num_kernel_sizes = len(kernel_sizes)
-num_rows = np.ceil(num_kernel_sizes / 2.0).astype(int)
 
 for image_name in image_names:
-    for filter_name in filters:
+    # Filter the dataframe by image_name
+    df_image = df[df.image_name == image_name]
+    
+    # get the unique values of the filter_name column
+    filter_names = df_image.filter_name.unique()
+    for filter_name in filter_names:
+        # Filter the dataframe by filter_name
+        df_filter = df_image[df_image.filter_name == filter_name]
+
+        # get the unique values of the kernel_size column
+        kernel_sizes = df_filter.kernel_size.unique()
+        num_kernel_sizes = len(kernel_sizes)
+        num_rows = np.ceil(num_kernel_sizes / 2.0).astype(int)
+
         subplot_index = 1
         fig, ax = plt.subplots(nrows=num_rows, ncols=2)
 
         # Split the filter_name into words and capitalize the first letter of each word
         title = ' '.join([word.capitalize() for word in filter_name.split('_')])
-
         # set the title of the figure
         fig.suptitle(f'{title} with varying Kernal Sizes', fontsize=14)
 
@@ -41,7 +47,7 @@ for image_name in image_names:
         subplot_index += 1
 
         for kernel_size in kernel_sizes:
-            df_filter = df[df.filter_name == filter_name]
+            # Filter the dataframe by kernel_size
             record = df_filter[df_filter.kernel_size == kernel_size]
             record = record.iloc[0]
 
@@ -58,4 +64,5 @@ for image_name in image_names:
             subplot_index += 1
         plt.savefig(f'./img/{image_name}-{filter_name}.png', bbox_inches='tight', dpi=600)
         plt.clf()
+        plt.close()
 
