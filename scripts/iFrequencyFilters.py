e1ff79f45355766d83c368faadd152a35d083a11
George Madeley <george.madeley@outlook.com>
2023-11-30 15:17:24 +0000

    Capped Interfaces to 80 Characters per Line

--- scripts/iFrequencyFilters.py
+++ scripts/iFrequencyFilters.py
@@ -1,25 +1,27 @@
 from abc import ABC, abstractmethod
 
+
 class IFrequencyFilters(ABC):
     @abstractmethod
     def applyFilter(self, image, filter_name: str, kernel_size: int, **kwargs):
         """
         Applies a linear filter to an image
-        
+
         :param image: The image to be filtered
         :param filter_name: The name of the filter
         :param kernel_size: The size of the kernel
         :param kwargs: The arguments for the filter
-        
+
         :return: The filtered image
         """
         pass
 
     @abstractmethod
-    def calculateFrequencyDomainConvolution(self, image, kernel, padding='constant'):
+    def calculateFrequencyDomainConvolution(
+            self, image, kernel, padding='constant'):
         """
-        Performs a convolution on an image using a kernel using the Fast Fourier Transform
-        algorithm.
+        Performs a convolution on an image using a kernel using the Fast Fourier
+        Transform algorithm.
 
         :param image: The image to be convolved
         :param kernel: The kernel to convolve the image with
--- scripts/iSpatialFilters.py
+++ scripts/iSpatialFilters.py
@@ -1,30 +1,37 @@
 from abc import ABC, abstractmethod
 
+
 class ISpatialFilters(ABC):
     @abstractmethod
     def applyFilter(self, image, filter_name: str, kernel_size: int, **kwargs):
         """
         Applies a linear filter to an image
-        
+
         :param image: The image to be filtered
         :param filter_name: The name of the filter
         :param kernel_size: The size of the kernel
         :param kwargs: The arguments for the filter
-        
+
         :return: The filtered image
         """
         pass
 
     @abstractmethod
-    def calculateSpatialDomainConvolution(self, image, kernel_size, filter_function, padding='constant'):
+    def calculateSpatialDomainConvolution(
+            self,
+            image,
+            kernel_size,
+            filter_function,
+            padding='constant'):
         """
-        Performs a convolution on an image using a kernel using the spatial domain algorithm.
-        
+        Performs a convolution on an image using a kernel using the spatial
+        domain algorithm.
+
         :param image: The image to be convolved
         :param kernel_size: The size of the kernel
         :param filter_function: The filter function to be applied
         :param padding: The type of padding to use. Possible values:
-        
+
         :return: The convolved image
         """
         pass
